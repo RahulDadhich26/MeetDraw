d34af90522a51842c61b3db36c2ee3b2f94a4df3
TheGaurdiann <rahuldadhich80263@gmail.com>
2025-08-27 18:45:46 +0530

    Backend deployed

--- excalidraw-clone-main/frontend/config.ts
+++ excalidraw-clone-main/frontend/config.ts
@@ -1,5 +1,3 @@
-// export const BACKEND_URL = "https://excalidraw-clone-ia8f.onrender.com";
-// export const WSS_URL = "wss://excalidraw-clone-ia8f.onrender.com"; 
+export const BACKEND_URL = "https://meetdraw.onrender.com";
+export const WSS_URL = "wss://meetdraw.onrender.com"; 
 
-export const BACKEND_URL = "http://localhost:2000";
-export const WSS_URL = "wss://localhost:2000"; 
